9bbf402dc0e54d0df4c3b2836b2d622c69c6f494
MaLiHa1321 <malihasultana2001@gmail.com>
2023-11-27 21:42:50 +0600

    pagination method are modified

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
       
     const database = client.db("inventory");
     const shopCollection = database.collection("shop");
@@ -194,7 +194,7 @@ async function run() {
     const result = await cursor.toArray();
 
     // Count total documents
-    const total = await userCollection.countDocuments();
+    const total = await userCollection.estimatedDocumentCount();
 
     res.send({
       total,
@@ -382,8 +382,8 @@ app.post('/jwt', async(req,res) =>{
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
